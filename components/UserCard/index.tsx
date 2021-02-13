8f4ecf1125acfcc3d00c9c34641f051a6bbc9b6a
Rafael Mujica <rdmujica86@gmail.com>
2021-02-13 16:19:51 -0300

    feat: add usercard component

---
+++ components/UserCard/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { Image, Label, Div, Anchor, LabelRepo, DivRepo } from './styles'
+
+const DEFAULT_IMAGE = ''
+
+interface IUser {
+  cover?: string
+  path?: string
+  name?: string
+  repository?: string
+}
+
+export const User = ({
+  cover = DEFAULT_IMAGE,
+  path = '#',
+  name = '',
+  repository = ''
+}: IUser) => (
+  <Link href={`/about?login=${name}`} passHref>
+    <Anchor>
+      <Div>
+        <Image src={cover} alt={name} />
+        <Label>{`@${name}`}</Label>
+        {repository && (
+          <DivRepo>
+            <LabelRepo>{repository}</LabelRepo>
+          </DivRepo>
+        )}
+      </Div>
+    </Anchor>
+  </Link>
+)
---
+++ components/UserCard/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const Anchor = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+`
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 230px;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #dde3ed;
+  border-radius: 20px;
+  box-shadow: 0px 10px 18px rgba(0, 0, 0, 0.2);
+  margin: 10px;
+`
+export const Image = styled.img`
+  box-shadow: 0px 10px 14px rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  overflow: hidden;
+  object-fit: cover;
+  width: 200px;
+  height: 200px;
+`
+
+export const Label = styled.span`
+  font-size: 18px;
+  margin-top: 10px;
+  text-align: center;
+  font-weight: bold;
+  color: #000;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  hyphens: auto;
+`
+export const DivRepo = styled.div`
+  background-color: #8d00ff;
+  border-radius: 10px;
+  padding: 4px 6px;
+  margin-top: 5px;
+  max-width: 210px;
+  text-align: center;
+`
+export const LabelRepo = styled.span`
+  font-size: 14px;
+  margin-top: 10px;
+  text-align: center;
+  font-weight: bold;
+  color: #fff;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  hyphens: auto;
+`
